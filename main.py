56506482874613898fe2d0e2a3ce4d1b4e0fef86
Paul Vatterott <pfvatterott@gmail.com>
2023-11-28 20:32:50 -0700

    created class for web scraper

--- main.py
+++ main.py
@@ -1,78 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from decouple import config
-from datetime import datetime
-import time
+from ski_history_scraper import SkiHistory
 
-web_id = config("web_id")
-email = config("email")
-password = config("password")
-
-alta_url = "https://shop.alta.com/customer/login"
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
-
-driver = webdriver.Chrome(options=chrome_options)
-
-driver.get(alta_url)
-
-
-# Logging in
-driver.find_element(By.XPATH, value='//*[@id="checkout-login-email"]').send_keys(email)
-driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div/div/div/div/form/div[2]/button').click()
-time.sleep(1)
-driver.find_element(By.XPATH, value='//*[@id="checkout-login-password"]').send_keys(password)
-driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div/div/div/div/form/div[2]/button').click()
-
-time.sleep(1)
-
-# Enter Web ID
-driver.find_element(By.XPATH, value='//*[@id="wtp-0"]').send_keys(web_id)
-driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div[3]/div/div/div[1]/div/div/div/form/div/div[2]/div/div/div/button').click()
-
-time.sleep(3)
-
-# Get Ski History
-driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div[3]/div/div/div[1]/div/div/div[3]/div/h4/a/i').click()
-time.sleep(3)
-total_ft_and_days = driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div[3]/div/div/div[1]/div/div/div[3]/div/h5')
+ski_history = SkiHistory()
+ski_history.login()
+ski_history.enter_web_id()
+total_ft_and_days = ski_history.get_ski_history()
 
 # Yearly Totals
 total_days = total_ft_and_days.text.split("SKIED: ")[1]
 total_ft = total_ft_and_days.text.split("TOTAL VERTICAL FEET ")[1].split(", NUMBER")[0]
 average_ft_per_day = int(total_ft.replace(",", "")) / int(total_days)
 
-# Individual day totals
-day_list = []
-each_day = driver.find_elements(By.CSS_SELECTOR, ".card-body .row .col-12 div h6")
-for day in each_day:
-    feet = int(day.text.split("\n")[0].split("VERTICAL FEET ")[1].replace(",", ""))
-    date = day.text.split("\n")[1].split(", ")[1]
-    date = date.lower().replace("rd", "").replace("nd", "").replace("st", "").replace("th", "")
-    date = datetime.strptime(date, "%B %d %Y").strftime("%m/%d/%Y")
-    obj = {
-        "date": date,
-        "feet": feet
-    }
-    day_list.append(obj)
+day_list = ski_history.get_each_day()
     
 # Runs Each Day
-runs_each_day = driver.find_elements(By.CSS_SELECTOR, ".card-body .row .col-12 div .table-responsive table")
-runs_in_day_index = 0
-for runs in runs_each_day:
-    each_run = runs.text.split("\n")
-    run_array = []
-    for run in each_run:
-        if "Lift Time" not in run:
-            obj = {
-                "lift": run.split(" ")[0],
-                "time": run.split(" ")[1] + " " + run.split(" ")[2]
-            }
-            run_array.append(obj)
-    day_list[runs_in_day_index]["runs"] = run_array
-    runs_in_day_index += 1
-    
+day_list = ski_history.get_runs_each_day(day_list)
 print(day_list)
             
         
---
+++ ski_history_scraper.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from decouple import config
+from datetime import datetime
+import time
+
+web_id = config("web_id")
+email = config("email")
+password = config("password")
+
+alta_url = "https://shop.alta.com/customer/login"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+class SkiHistory:
+    def __init__(self):
+        self.driver = webdriver.Chrome(options=chrome_options)
+        
+    
+    def login(self):
+        self.driver.get(alta_url)
+        self.driver.find_element(By.XPATH, value='//*[@id="checkout-login-email"]').send_keys(email)
+        self.driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div/div/div/div/form/div[2]/button').click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, value='//*[@id="checkout-login-password"]').send_keys(password)
+        self.driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div/div/div/div/form/div[2]/button').click()
+        time.sleep(1)
+        
+    def enter_web_id(self):
+        self.driver.find_element(By.XPATH, value='//*[@id="wtp-0"]').send_keys(web_id)
+        self.driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div[3]/div/div/div[1]/div/div/div/form/div/div[2]/div/div/div/button').click()
+        time.sleep(3)
+        
+    def get_ski_history(self):
+        self.driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div[3]/div/div/div[1]/div/div/div[3]/div/h4/a/i').click()
+        time.sleep(3)
+        return self.driver.find_element(By.XPATH, value='//*[@id="app"]/main/section/div[3]/div/div/div[1]/div/div/div[3]/div/h5')
+        
+    def get_each_day(self):
+        day_list = []
+        each_day = self.driver.find_elements(By.CSS_SELECTOR, ".card-body .row .col-12 div h6")
+        for day in each_day:
+            feet = int(day.text.split("\n")[0].split("VERTICAL FEET ")[1].replace(",", ""))
+            date = day.text.split("\n")[1].split(", ")[1]
+            date = date.lower().replace("rd", "").replace("nd", "").replace("st", "").replace("th", "")
+            date = datetime.strptime(date, "%B %d %Y").strftime("%m/%d/%Y")
+            obj = {
+                "date": date,
+                "feet": feet
+            }
+            day_list.append(obj)
+        return day_list
+    
+    def get_runs_each_day(self, day_list):
+        runs_each_day = self.driver.find_elements(By.CSS_SELECTOR, ".card-body .row .col-12 div .table-responsive table")
+        runs_in_day_index = 0
+        for runs in runs_each_day:
+            each_run = runs.text.split("\n")
+            run_array = []
+            for run in each_run:
+                if "Lift Time" not in run:
+                    obj = {
+                        "lift": run.split(" ")[0],
+                        "time": run.split(" ")[1] + " " + run.split(" ")[2] 
+                    }
+                    run_array.append(obj)
+            day_list[runs_in_day_index]["runs"] = run_array
+            runs_in_day_index += 1
+            
+        return day_list
+        
+        
