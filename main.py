b3793d9d34b0751dee062dd46d7e99a27f4f58d7
Paul Vatterott <pfvatterott@gmail.com>
2023-11-30 19:41:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from ski_history_scraper import SkiHistory
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///alta_web_scrapper.db"
+db = SQLAlchemy()
+db.init_app(app)
+
+class Users(db.Model):
+    userId = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    userName = db.Column(db.String, nullable=False, unique=True)
+    web_id = db.Column(db.String, nullable=False, unique=True)
+    yearly_elevation = db.Column(db.Integer, nullable=False)
+    days_skied = db.Column(db.Integer, nullable=False)
+    average_ft = db.Column(db.Integer, nullable=False)
+    
+class DailySkiData(db.Model):
+    dailyDataId = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    daily_elevation = db.Column(db.Integer, nullable=False)
+    daily_runs = db.Column(db.Integer, nullable=False)
+    
+class DailySkiData(db.Model):
+    runDataId = db.Column(db.Integer, primary_key=True)
+    dailyDataId = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    run_elevation = db.Column(db.Integer, nullable=False)
+    chairlift = db.Column(db.String, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    
+
 
 ski_history = SkiHistory()
 ski_history.login()
